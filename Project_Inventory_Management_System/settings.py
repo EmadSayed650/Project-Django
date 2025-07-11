3d8c35022e0cf79965649f06786fc4e16b57330e
MohamedAbdrabou12 <mohamedabdrabou840@gmail.com>
2025-07-11 21:48:34 +0300

    edit setting for production

--- Project_Inventory_Management_System/settings.py
+++ Project_Inventory_Management_System/settings.py
@@ -174,13 +174,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static", 
+#     BASE_DIR / "home/static",  
+# ]
+# STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static", 
-    BASE_DIR / "home/static",  
+    str(BASE_DIR / "static"), 
+    str(BASE_DIR / "home/static"),  
 ]
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
-
+STATIC_ROOT = '/home/mohamedabdo/Project-Django/staticfiles'
 
 
 
