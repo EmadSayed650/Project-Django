5c2027b4ec135e81285ac31a21643190133a9f56
shrouk <shroukkhamis239@gmail.com>
2025-07-11 08:44:53 +0300

    add model of order that contain all properties

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 import uuid
 from users.models import CustomUser  
-# Create your models here.
 
 class Supermarket(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
@@ -11,10 +10,18 @@ class Supermarket(models.Model):
         return f"Supermarket {self.id}: {self.name} located at {self.location}"
 
 class Order(models.Model):
-    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Confirmed', 'Confirmed'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled')
+    ]
+
+    order_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     supermarket = models.ForeignKey(Supermarket, on_delete=models.CASCADE)
     order_date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Confirmed', 'Confirmed'), ('Shipped', 'Shipped'), ('Delivered', 'Delivered'), ('Cancelled', 'Cancelled')], default='Pending')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
     created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True, related_name='created_orders')
     confirmed_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='confirmed_orders')
 
