d517f93caf9f0352076582dcb2838f67b6cb2a58
shrouk <shroukkhamis239@gmail.com>
2025-07-11 09:02:52 +0300

    merged manage_dashboard.html

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role',)}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active')
+    
